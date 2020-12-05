a84cfa2b0be25d2caa0cfe24833da220fc6fb06c
David S <dvsalcedov@gmail.com>
2020-12-05 11:54:02 -0500

    Fix in number of layers

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace RotateMatrix
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } , { 7, 8, 9 } };
-            int[,] matrix = new int[,] { { 1, 2, 3 , 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+            int[,] matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } , { 7, 8, 9 } };
+            //int[,] matrix = new int[,] { { 1, 2, 3 , 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for(int j = 0; j < matrix.GetLength(0); j++)
@@ -35,16 +35,13 @@ namespace RotateMatrix
             int length = matrix.GetLength(0);
             int start = 0;
             int final = length-1;
-            for (int layer = 0; layer < length; layer++) {
+            int tempTop;
+            for (int layer = 0; layer < length/2; layer++) {
                 start = start + layer;
                 final = final - layer;
-                length = length - 1;
-                int[] temp = new int[length + 1];
-                for (int i = 0; i <= length; i++)
-                    temp[i] = matrix[start, i];
 
                 for (int i = start; i < final; i++){
-
+                    tempTop = matrix[start, i];
                     //Left to Top
                     matrix[start, i] = matrix[final - i , start ]; ;
 
@@ -55,9 +52,8 @@ namespace RotateMatrix
                     matrix[final, final - i] = matrix[i, final];
 
                     //Top to Right
-                    matrix[i,final] = temp[i];
-                    
-                                    
+                    matrix[i,final] = tempTop;
+                                                        
                 }
 
             }
